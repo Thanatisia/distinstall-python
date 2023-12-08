53bf21c21865dbb781e0e5546a030459089a3bbd
Thanatisia <55834101+Thanatisia@users.noreply.github.com>
2023-12-08 22:48:53 +0800

    Update 2023-12-08 2248H - Added a simple run.sh script to automate the process

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+cd src && \
+    python main.py \
+    -m RELEASE \
+    -u /dev/sdb \
+    --execute-stage 1 \
+    --execute-stage 2 \
+    --execute-stage 3 \
+    --execute-stage 4 \
+    --execute-stage 5 \
+    --execute-stage 6 \
+    --execute-stage 7 \
+    --execute-stage 8 \
+    --execute-stage 9 \
+    --execute-stage 10 \
+    start
